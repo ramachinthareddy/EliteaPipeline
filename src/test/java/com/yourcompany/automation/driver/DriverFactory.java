f7b5d9fe164881c3ca134fea12e06e1c3714d4e7
RamaChinthareddy <ramachinthareddy545@gmail.com>
2026-06-09 15:27:42 +0530

    Create src/test/java/com/yourcompany/automation/driver/DriverFactory.java

---
+++ src/test/java/com/yourcompany/automation/driver/DriverFactory.java
@@ -0,0 +1,25 @@
+package com.yourcompany.automation.driver;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class DriverFactory {
+    private static final ThreadLocal<WebDriver> driver = new ThreadLocal<>();
+
+    public static WebDriver getDriver() {
+        if (driver.get() == null) {
+            WebDriverManager.chromedriver().setup();
+            driver.set(new ChromeDriver());
+            driver.get().manage().window().maximize();
+        }
+        return driver.get();
+    }
+
+    public static void quitDriver() {
+        if (driver.get() != null) {
+            driver.get().quit();
+            driver.remove();
+        }
+    }
+}
