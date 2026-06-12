adcc9651e36bfa5d7258c39443b66c047c8e0cc2
RamaChinthareddy <ramachinthareddy545@gmail.com>
2026-06-12 14:12:23 +0530

    Replace BasePage implementation with PageFactory-backed version

--- src/main/java/com/elitea/pages/BasePage.java
+++ src/main/java/com/elitea/pages/BasePage.java
@@ -1,28 +1,22 @@
 package com.elitea.pages;
 
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
-import java.time.Duration;
+import org.openqa.selenium.support.PageFactory;
 
 public abstract class BasePage {
     protected final WebDriver driver;
-    protected final WebDriverWait wait;
 
-    protected BasePage(WebDriver driver) {
+    public BasePage(WebDriver driver) {
         this.driver = driver;
-        this.wait = new WebDriverWait(driver, Duration.ofSeconds(15));
+        PageFactory.initElements(driver, this);
     }
 
-    protected void waitForVisible(org.openqa.selenium.By locator) {
-        wait.until(ExpectedConditions.visibilityOfElementLocated(locator));
+    protected void jsClick(Object element) {
+        ((JavascriptExecutor) driver).executeScript("arguments[0].click();", element);
     }
 
-    protected void waitForClickable(org.openqa.selenium.By locator) {
-        wait.until(ExpectedConditions.elementToBeClickable(locator));
-    }
-
-    protected String getCurrentUrl() {
-        return driver.getCurrentUrl();
+    public String getTitle() {
+        return driver.getTitle();
     }
 }
